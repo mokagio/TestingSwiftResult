cca8c6d59838bac09dbabe90f067d0a034f412a5
Gio Lodi <giovanni.lodi42@gmail.com>
2020-12-02 23:17:31 +1100

    Add more test helpers

--- Tests/XCTestCase+Result.swift
+++ Tests/XCTestCase+Result.swift
@@ -2,6 +2,21 @@ import XCTest
 
 extension XCTestCase {
 
+    func assertIsSuccess<T, E>(
+        _ result: Result<T, E>,
+        then assertions: (T) -> Void = { _ in },
+        message: (E) -> String = { "Expected to be a success but got a failure with \($0) "},
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) where E: Error {
+        switch result {
+        case .failure(let error):
+            XCTFail(message(error), file: file, line: line)
+        case .success(let value):
+            assertions(value)
+        }
+    }
+
     func assert<T, E>(
         _ result: Result<T, E>,
         isSuccessWith value: T,
@@ -14,6 +29,21 @@ extension XCTestCase {
             XCTFail(message(error), file: file, line: line)
         case .success(let resultValue):
             XCTAssertEqual(resultValue, value)
+        }
+    }
+
+    func assertIsFailure<T, E>(
+        _ result: Result<T, E>,
+        then assertions: (E) -> Void = { _ in },
+        message: (T) -> String = { "Expected to be a failure but got a success with \($0) "},
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) where E: Equatable & Error {
+        switch result {
+        case .failure(let error):
+            assertions(error)
+        case .success(let value):
+            XCTFail(message(value), file: file, line: line)
         }
     }
 
