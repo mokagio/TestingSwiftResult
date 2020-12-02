2cca1da382d0e9c92a3ceda8e65b0799dd3b1111
Gio Lodi <giovanni.lodi42@gmail.com>
2020-12-02 22:54:30 +1100

    Update tests to be in line with the companion blogpost

--- Tests/ResultTests.swift
+++ Tests/ResultTests.swift
@@ -2,72 +2,75 @@ import XCTest
 
 class ResultTests: XCTestCase {
 
-    func testResultSuccess() throws {
-        let result = makeResult(42)
+    func testResultSuccessGet() throws {
+        let result = Result<Int, Error>.success(42)
 
-        // Option 1
+        let value = try result.get()
+
+        XCTAssertEqual(value, 42)
+    }
+
+    func testResultSuccessExampleGuard() {
+        let result = Result<Int, Error>.success(42)
+
+        guard case .success(let value) = result else {
+            return XCTFail("Expected to be a success but got a failure with \(result)")
+        }
+        XCTAssertEqual(value, 42)
+    }
+
+    func testResultSuccessExampleSwitch() throws {
+        // Wrapped in closure to avoid warning about switch condition on a constant.
+        let result = { Result<Int, Error>.success(42) }()
+
         switch result {
         case .success(let value):
             XCTAssertEqual(value, 42)
         case .failure(let error):
-            XCTFail("Expected to be a .success but got a .failure with \(error)")
+            XCTFail("Expected to be a success but got a failure with \(error)")
         }
 
-        // Option 2
-        guard case .success(let value) = result else {
-            return XCTFail("Expected to be a .success but got a .failure with \(result)")
-        }
-        XCTAssertEqual(value, 42)
-
-        // Option 3
-        let value_ = try result.get()
-        XCTAssertEqual(value_, 42)
-
-        // Option 3
         assert(result, isSuccessWith: 42)
     }
 
-    func testResultFailure() {
-        let error = TestError()
-        let result = makeResult(error)
+    func testResultSuccessWithHelper() {
+        let result = Result<Int, TestError>.success(42)
 
-        // Option 1
+        assert(result, isSuccessWith: 42)
+        assert(result, isSuccessWith: 42, message: { "Failed with '\($0.message)'" })
+    }
+
+    func testResultFailureGuard() {
+        let expectedError = TestError()
+        let result = Result<Never, TestError>.failure(expectedError)
+
+        guard case .failure(let error) = result else {
+            return XCTFail("Expected to be a failure but got a success with \(result)")
+        }
+        XCTAssertEqual(error, expectedError)
+    }
+
+    func testResultFailureSwitch() {
+        let expectedError = TestError()
+        // Wrapped in closure to avoid warning about switch condition on a constant.
+        let result = { Result<Never, TestError>.failure(expectedError) }()
+
         switch result {
         case .success(let value):
-            XCTFail("Expected to be a .failure but got a .success with \(value)")
-        case .failure(let resultError):
-            XCTAssertEqual(resultError, error)
+            XCTFail("Expected to be a failure but got a success with \(value)")
+        case .failure(let error):
+            XCTAssertEqual(error, expectedError)
         }
-
-        // Option 2
-        guard case .failure(let resultError) = result else {
-            return XCTFail("Expected to be a .failure but got a .success with \(result)")
-        }
-        XCTAssertEqual(resultError, error)
-
-        // Option 3
-        assert(result, isFailureWith: error)
     }
 
-    // MARK: -
+    func testResultFailureWithHelper() {
+        let result = Result<Int, TestError>.failure(TestError(message: "abc"))
 
-    // These are just to avoid doing a
-    //
-    // ```
-    // let result = Result<...>.success(...)
-    // ```
-    //
-    // and then get a warning from Xcode:
-    //
-    // > Switch condition evaluates a constant
-
-    func makeResult<T>(_ value: T) -> Result<T, Error> {
-        return .success(value)
-    }
-
-    func makeResult<E>(_ error: E) -> Result<Never, E> where E: Error {
-        return .failure(error)
+        assert(result, isFailureWith: TestError(message: "abc"))
+        assert(result, isFailureWith: TestError(message: "abc"), message: { "Succeeded with '\($0)'" })
     }
 }
 
-struct TestError: Equatable, Error {}
+struct TestError: Equatable, Error {
+    private(set) var message: String = "unknown"
+}
