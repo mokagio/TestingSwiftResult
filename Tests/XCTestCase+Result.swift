88cb080f67809e3a2f86063205e408ac610556e6
Gio Lodi <giovanni.lodi42@gmail.com>
2020-12-02 23:00:30 +1100

    Make test helpers report inline

--- Tests/XCTestCase+Result.swift
+++ Tests/XCTestCase+Result.swift
@@ -4,25 +4,31 @@ extension XCTestCase {
 
     func assert<T, E>(
         _ result: Result<T, E>,
-        isSuccessWith value: T
+        isSuccessWith value: T,
+        message: (E) -> String = { "Expected to be a success but got a failure with \($0) "},
+        file: StaticString = #filePath,
+        line: UInt = #line
     ) where E: Error, T: Equatable {
         switch result {
+        case .failure(let error):
+            XCTFail(message(error), file: file, line: line)
         case .success(let resultValue):
             XCTAssertEqual(resultValue, value)
-        case .failure(let error):
-            XCTFail("Expected to be a .success but got a .failure with \(error)")
         }
     }
 
     func assert<T, E>(
         _ result: Result<T, E>,
-        isFailureWith error: E
+        isFailureWith error: E,
+        message: (T) -> String = { "Expected to be a failure but got a success with \($0) "},
+        file: StaticString = #filePath,
+        line: UInt = #line
     ) where E: Equatable & Error {
         switch result {
-        case .success(let value):
-            XCTFail("Expected to be a .failure but got a .success with \(value)")
         case .failure(let resultError):
             XCTAssertEqual(resultError, error)
+        case .success(let value):
+            XCTFail(message(value), file: file, line: line)
         }
     }
 }
